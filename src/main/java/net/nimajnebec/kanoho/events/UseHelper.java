638ebbc8dddd976fba064db2ab2ce0194975ff89
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-10-08 20:13:23 +0100

    fix: respect mainhand/offhand priorities

--- src/main/java/net/nimajnebec/kanoho/events/UseHelper.java
+++ src/main/java/net/nimajnebec/kanoho/events/UseHelper.java
@@ -4,11 +4,15 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.nimajnebec.kanoho.Kanoho;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_20_R1.inventory.CraftItemStack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.PlayerInventory;
 
 public class UseHelper implements Listener {
     public static final String TAG_USED = "used";
@@ -18,16 +22,29 @@ public class UseHelper implements Listener {
         this.plugin = plugin;
     }
 
+    public boolean isUsable(org.bukkit.inventory.ItemStack item) {
+        if (item.getType() == Material.AIR) return false;
+        return isUsable(((CraftItemStack) item).handle.getOrCreateTag());
+    }
+
     public boolean isUsable(CompoundTag nbt) {
         return nbt.contains(TAG_USED);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerUse(PlayerInteractEvent event) {
-        // TODO: Add checks to ensure only one item is used at a time
-        org.bukkit.entity.Player player = event.getPlayer();
-
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         if (event.getItem() == null) return;
+
+        // TODO: Do not use if clicking on interactive block
+
+        // Cancel use of offhand item if mainhand is usable
+        PlayerInventory inventory = event.getPlayer().getInventory();
+        if (event.getHand() == EquipmentSlot.OFF_HAND && isUsable(inventory.getItem(EquipmentSlot.HAND))) {
+            event.setCancelled(true);
+            return;
+        }
+
         ItemStack item = ((CraftItemStack) event.getItem()).handle;
         CompoundTag nbt = item.getOrCreateTag();
 
