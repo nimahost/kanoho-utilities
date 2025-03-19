0de152937035b6e1386225cbb8a40bfeb52bd6ff
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-03-19 16:06:13 +0000

    feat: don't play if consume time 0

--- src/main/java/ec/brooke/kanoho/vanilla/mixins/ConsumableMixin.java
+++ src/main/java/ec/brooke/kanoho/vanilla/mixins/ConsumableMixin.java
@@ -2,12 +2,16 @@ package ec.brooke.kanoho.vanilla.mixins;
 
 import ec.brooke.kanoho.components.KanohoComponents;
 import ec.brooke.kanoho.vanilla.IServerPlayer;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -29,4 +33,14 @@ public abstract class ConsumableMixin {
         }
     }
 
+    @Accessor
+    public abstract float getConsumeSeconds();
+
+    // If the consume time is zero, set to non-existent namespace to prevent
+    @Inject(method = "sound", cancellable = true, at = @At(value = "RETURN"))
+    private void cancelConsume(CallbackInfoReturnable<Holder<SoundEvent>> cir) {
+        if (getConsumeSeconds() != 0) return;
+        ResourceLocation rl = ResourceLocation.fromNamespaceAndPath("", "");
+        cir.setReturnValue(new Holder.Direct<>(SoundEvent.createFixedRangeEvent(rl, 0)));
+    }
 }
