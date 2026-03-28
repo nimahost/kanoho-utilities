65a938afa6781ddcb4783ccaa10377f1c8d80d63
Brooke <46959407+nimajnebec@users.noreply.github.com>
2026-03-28 23:26:36 +0000

    fix: double prop placement

--- src/main/java/ec/brooke/kanoho/features/props/PropPlacer.java
+++ src/main/java/ec/brooke/kanoho/features/props/PropPlacer.java
@@ -1,7 +1,6 @@
 package ec.brooke.kanoho.features.props;
 
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
@@ -12,7 +11,6 @@ import net.minecraft.world.entity.Display;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
@@ -25,30 +23,12 @@ public class PropPlacer {
     private static final SoundEvent SOUND = SoundEvents.ARMOR_STAND_PLACE;
 
     public void register() {
-        UseItemCallback.EVENT.register(this::onUseItem);
         UseBlockCallback.EVENT.register(this::onUseBlock);
     }
 
     private InteractionResult onUseBlock(Player player, Level level, InteractionHand hand, BlockHitResult hit) {
-        return place(player, level, hand);
-    }
-
-    private InteractionResult onUseItem(Player player, Level level, InteractionHand hand) {
-        return place(player, level, hand);
-    }
-
-    private static InteractionResult place(Player player, Level level, InteractionHand hand) {
         if (!player.mayBuild() || !PROP.from(player.getItemInHand(hand)).orElse(false)) return InteractionResult.PASS;
 
-        ClipContext context = new ClipContext(
-                player.getEyePosition(),
-                player.getLookAngle().scale(5).add(player.getEyePosition()),
-                ClipContext.Block.OUTLINE,
-                ClipContext.Fluid.NONE,
-                player
-        );
-
-        BlockHitResult hit = level.clip(context);
         if (hit.getType() == BlockHitResult.Type.MISS) return InteractionResult.PASS;
         Vec3 position = hit.getLocation()
                 .subtract(player.getLookAngle().scale(0.01))
@@ -67,5 +47,4 @@ public class PropPlacer {
         level.addFreshEntity(entity);
         return InteractionResult.SUCCESS;
     }
-
 }
